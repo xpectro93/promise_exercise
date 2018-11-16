c610548a5a758eac3a011c39346b440a5c94ec40
your name here <your_email_here>
2018-11-16 14:14:09 -0500

    work in progress

---
+++ promise_assignment.js
@@ -0,0 +1,24 @@
+// let promise = new Promise(function(num, resolve, reject) {
+//   // do a thing, possibly async, then…
+//
+//   if (!num || num > 10) {
+//     resolve("Stuff worked!");
+//   } else {
+//     reject(Error("It broke"));
+//   }
+// }).then(res => {
+//   console.log(res);
+// });
+// promise(9);
+// // console.log(promise);
+
+//////part 1/////////
+// function testNum(num) {
+//   return new Promise((resolve, reject) => {
+//     resolve(num > 10);
+//   });
+// }
+//
+// console.log(testNum(9));
+
+//////Part 2/////////
